7f37e176d07fa7c19ef8e083b52659afc8c4e4e3
koljapluemer <github@koljapluemer.com>
2025-04-14 18:27:55 +0200

    rework video overview to something more sane

--- frontend/src/modules/videos/video-list/VideoList.vue
+++ frontend/src/modules/videos/video-list/VideoList.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
 import { getVideos } from '@/modules/backend-communication/api'
+import VideoTile from './VideoTile.vue'
 
 const videoIds = ref<string[]>([])
 const loading = ref(true)
 const error = ref<string | null>(null)
-const router = useRouter()
 
 const fetchVideos = async () => {
   loading.value = true
@@ -32,26 +31,12 @@ onMounted(() => {
       <span>{{ error }}</span>
     </div>
 
-    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div v-for="videoId in videoIds" :key="videoId" class="card bg-base-100 shadow-xl">
-        <figure class="px-4 pt-4">
-          <img 
-            :src="`https://img.youtube.com/vi/${videoId}/mqdefault.jpg`" 
-            :alt="`Video thumbnail for ${videoId}`"
-            class="rounded-xl"
-          />
-        </figure>
-        <div class="card-body">
-          <div class="card-actions justify-end mt-4">
-            <button 
-              class="btn btn-primary"
-              @click="router.push({ name: 'video', params: { videoId }})"
-            >
-              View Video
-            </button>
-          </div>
-        </div>
-      </div>
+    <div v-else class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
+      <VideoTile
+        v-for="videoId in videoIds"
+        :key="videoId"
+        :video-id="videoId"
+      />
     </div>
   </div>
 </template>
---
+++ frontend/src/modules/videos/video-list/VideoTile.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const props = defineProps<{
+  videoId: string
+}>()
+
+const router = useRouter()
+
+const handleViewVideo = () => {
+  router.push({ name: 'video', params: { videoId: props.videoId }})
+}
+</script>
+
+<template>
+  <div class="group relative cursor-pointer max-w-[240px] mx-auto" @click="handleViewVideo">
+    <div class="relative overflow-hidden rounded-lg">
+      <img 
+        :src="`https://img.youtube.com/vi/${videoId}/mqdefault.jpg`" 
+        :alt="`Video thumbnail for ${videoId}`"
+        class="w-full transition-transform duration-300 group-hover:scale-105"
+      />
+      <div class="absolute inset-0 bg-black/20 group-hover:bg-black/30 transition-colors duration-300" />
+    </div>
+  </div>
+</template>
