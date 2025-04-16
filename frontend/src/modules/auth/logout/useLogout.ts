a2c3b60ed0976e8e7772d5a9b1a327d2fb06f776
koljapluemer <github@koljapluemer.com>
2025-04-16 12:43:01 +0200

    add redirect to logout

--- frontend/src/modules/auth/logout/useLogout.ts
+++ frontend/src/modules/auth/logout/useLogout.ts
@@ -1,15 +1,16 @@
 import { useRouter } from 'vue-router'
 import { useAuthState } from '@/modules/backend-communication/api'
+import { useToast } from '@/shared/elements/toast/useToast'
 
 export const useLogout = () => {
   const router = useRouter()
   const { logout } = useAuthState()
+  const { success } = useToast()
 
   const handleLogout = () => {
     logout()
-    if (router.currentRoute.value.name !== 'home') {
-      router.push({ name: 'home' })
-    }
+    success('Successfully logged out')
+    router.push({ name: 'login' })
   }
 
   return {
